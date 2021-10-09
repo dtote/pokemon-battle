b70a28884c12129da3f91b4da8bffb9c8d2ee6f4
Nestor Torres <alu0101021768@ull.edu.es>
2021-10-09 22:56:15 +0100

    Fixed all problems

--- src/js/index.js
+++ src/js/index.js
@@ -1,83 +1,68 @@
 import _ from "lodash";
 
+const createHtmlElement = (type) => document.createElement(type);
+
 // DOM
 const startButton = document.querySelector(".start-button");
 const [topPokemonPicDiv, bottomPokemonPicDiv] = document.querySelectorAll(".pokemon");
 const [topPokemonStatsDiv, bottomPokemonStatsDiv] = document.querySelectorAll(".poke-stats");
-
+const [image1, name1, attack1, defense1, hp1] =
+[
+  createHtmlElement("img"),
+  createHtmlElement("div"),
+  createHtmlElement("div"),
+  createHtmlElement("div"),
+  createHtmlElement("div")
+];
+const [image2, name2, attack2, defense2, hp2] =
+[
+  createHtmlElement("img"),
+  createHtmlElement("div"),
+  createHtmlElement("div"),
+  createHtmlElement("div"),
+  createHtmlElement("div")
+];
+const topBasicStats = [image1, name1, attack1, defense1, hp1];
+const bottomBasicStats = [image2, name2, attack2, defense2, hp2];
 const getTwoRandomNumbers = () => _.shuffle(_.range(1, 100)).slice(0, 2);
 
-const fetchPokemonData = async() => {
+const fetchPokemonData = (index) => {
   const pokemonApiPrefix = "https://pokeapi.co/api/v2/pokemon/";
-  const pokemonIndexes = getTwoRandomNumbers();
-  return await pokemonIndexes.map((index) => {
-    return fetch(`${pokemonApiPrefix + index}`)
-      .then((response) => response.json())
-      .then((data) => data);
-  });
+  return fetch(`${pokemonApiPrefix + index}`)
+    .then((response) => response.json())
+    .then((data) => data);
 };
 
+const setPokemonData = (pokemon, data, basicStats) => {
+  const basicStatsTextContent = [[], ["Name: ", data.name.toUpperCase()], ["Attack: ", data.stats[0].base_stat], ["HP: ", data.stats[1].base_stat], ["Defense: ", data.stats[2].base_stat]];
+  basicStats.forEach((stat, index) => {
+    if (index !== 0) {
+      stat.classList.add("stat");
+      stat.textContent = `${basicStatsTextContent[index][0] + basicStatsTextContent[index][1]}`;
+      pokemon.statsDiv.appendChild(stat);
+    } else {
+      stat.src = data.sprites.front_default;
+      stat.classList.add("pokemon-pic");
+      pokemon.picDiv.appendChild(stat);
+    }
+  });
+};
 const getAllPokemons = async() => {
-  // clearBattle();
-  // localStorage.setItem("pokemonIndexes", JSON.stringify(pokemonIndexes));
-  await pokemonIndexes.map((pokemonIndex) => {
-    return fetch(`${pokemonApiPrefix + pokemonIndex}`)
-      .then((response) => response.json())
-      .then((pokemon) => {
-        const pokemonData = {
-          name: pokemon.name.toUpperCase(),
-          hp: pokemon.stats[0].base_stat,
-          attack: pokemon.stats[1].base_stat,
-          defense: pokemon.stats[2].base_stat,
-          front_img: pokemon.sprites.front_default,
-          back_img: pokemon.sprites.back_default,
-        };
-        localStorage.setItem(pokemonIndex, JSON.stringify(pokemonData));
-      });
-  });
-  setPokemon(topPokemon);
-  setPokemon(bottomPokemon);
-};
+  const pokemonIndexes = getTwoRandomNumbers();
+  const topPokemonData = await fetchPokemonData(pokemonIndexes[0]);
+  const bottomPokemonData = await fetchPokemonData(pokemonIndexes[1]);
+  const topPokemon = {
+    picDiv: topPokemonPicDiv,
+    statsDiv: topPokemonStatsDiv
+  };
+  const bottomPokemon = {
+    picDiv: bottomPokemonPicDiv,
+    statsDiv: bottomPokemonStatsDiv
+  };
 
-const getStoredPokemonIndex = (pokemonIndex) => JSON.parse(localStorage.getItem("pokemonIndexes"))[pokemonIndex];
-const getStoredPokemon = (index) => JSON.parse(localStorage.getItem(getStoredPokemonIndex(index)));
-const topPokemon = {
-  data: getStoredPokemon(0),
-  picDiv: topPokemonPicDiv,
-  statsDiv: topPokemonStatsDiv
-};
-const bottomPokemon = {
-  data: getStoredPokemon(1),
-  picDiv: bottomPokemonPicDiv,
-  statsDiv: bottomPokemonStatsDiv
-};
-const clearBattle = () => {
-  localStorage.clear();
-  topPokemon.picDiv.childNodes.forEach((child) => child.remove());
-  bottomPokemon.picDiv.childNodes.forEach((child) => child.remove());
-  topPokemon.statsDiv.childNodes.forEach((child) => child.remove());
-  bottomPokemon.statsDiv.childNodes.forEach((child) => child.remove());
-};
-
-const setPokemon = (pokemon) => {
-  const pokemonImage = document.createElement("img");
-  const pokemonName = document.createElement("div");
-  const pokemonAttack = document.createElement("div");
-  const pokemonDefense = document.createElement("div");
-  const pokemonHp = document.createElement("div");
-  const basicStats = [pokemonName, pokemonAttack, pokemonHp, pokemonDefense];
-  pokemonName.textContent = "Name: " + pokemon.data.name;
-  pokemonAttack.textContent = "Attack: " + pokemon.data.attack;
-  pokemonHp.textContent = "HP: " + pokemon.data.hp;
-  pokemonDefense.textContent = "Defense: " + pokemon.data.defense;
-  pokemonImage.src = pokemon.data.front_img;
-  pokemonImage.classList.add("pokemon-pic");
-  pokemon.picDiv.appendChild(pokemonImage);
-  basicStats.forEach((stat) => {
-    stat.classList.add("stat");
-    pokemon.statsDiv.appendChild(stat);
-  });
+  setPokemonData(topPokemon, topPokemonData, topBasicStats);
+  setPokemonData(bottomPokemon, bottomPokemonData, bottomBasicStats);
 };
 
 // Listeners
-startButton.addEventListener("click", () => getAllPokemons());
+startButton.addEventListener("click", getAllPokemons);
